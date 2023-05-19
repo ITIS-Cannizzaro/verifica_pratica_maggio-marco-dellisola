e73db83834239f3acc216379de83219385097eed
utente <utente@DESKTOP-5GU5A7E>
2023-05-19 12:53:26 +0200

    verifica

---
+++ src/Main.java
@@ -0,0 +1,71 @@
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class Main extends JFrame implements ActionListener{
+	
+	private ArrayList<Integer> l;
+	
+	JTextArea aggiungi= new JTextArea();
+	JTextArea risultato= new JTextArea();
+	
+	JButton Aggiungi= new JButton("Aggiungi");
+	JButton Rimuovi= new JButton("Rimuovi");
+	JButton Visualizza= new JButton("Visualizza");
+	
+	JLabel Inserisci=new JLabel("Inserisci un numero:");
+	
+	public void funzioni() {
+		
+		l = new ArrayList<>();
+		
+		Container cont=this.getContentPane();
+		cont.setLayout(new FlowLayout());
+		cont.add(Aggiungi);
+		cont.add(aggiungi);
+		
+		
+		Aggiungi.setHorizontalTextPosition(Aggiungi.LEFT);
+		aggiungi.setPreferredSize(new Dimension(250,20));
+		Aggiungi.addActionListener(e -> junta());
+		
+		Visualizza.setHorizontalTextPosition(Visualizza.LEFT);
+		risultato.setPreferredSize(new Dimension(250,20));
+		
+		Rimuovi.setHorizontalTextPosition(Rimuovi.LEFT);
+		Rimuovi.addActionListener(e -> cancella());
+		
+		setSize(400,400);
+		setVisible(true);
+		setResizable(false);
+		
+	}
+	
+	public void junta() {
+		for(int i; i<l.size(); i++) {
+			
+		}
+	}
+	
+	public void cancella() {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==Visualizza) {
+			junta();
+		}
+		if(e.getSource()==Rimuovi) {
+			cancella();
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
